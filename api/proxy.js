2d3c44c4d1836c8b57a79dd17fdbc71e40a59d13
NOTAHACKER9999 <th15isar0bothi@gmail.com>
2026-03-10 19:27:30 -0500

    Delete api directory

--- api/proxy.js
+++
@@ -1,9 +0,0 @@
-import { createServer } from "@mercuryworkshop/scramjet";
-
-const server = createServer({
-  prefix: "/scramjet/"
-});
-
-export default async function handler(req, res) {
-  return server(req, res);
-}
